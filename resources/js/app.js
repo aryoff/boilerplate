3eb95dc085b30d29fce20a4f052c5a6a6fc96556
Aryo Fajar <aryoff@gmail.com>
2021-03-03 04:18:36 +0700

    update npm, add datatables vendor

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,7 @@ require('tempusdominus-bootstrap-4');
 require('daterangepicker');
 require('summernote');
 require('jquery-knob');
+window.DataTable = require('datatables.net-bs4');
 
 // require('jqvmap-novulnerability');
 // require('jqvmap-novulnerability/dist/maps/jquery.vmap.usa');
